f5fb2ddb2f7df132f9bc1493de4d4dc6678a904f
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-09-28 10:48:45 +0530

    added string parser

--- parser.js
+++ parser.js
@@ -7,7 +7,7 @@ function nullParser (input) {
   return null
 }
 
-console.log(nullParser('nullABC'))
+console.log('nullParser', nullParser('nullABC'))
 
 // booleanParser
 function booleanParser (input) {
@@ -20,18 +20,62 @@ function booleanParser (input) {
   return null
 }
 
-console.log(booleanParser('trueABC'))
+console.log('booleanParser', booleanParser('trueABC'))
 
 // number parser
 function numberParser (input) {
-  const regex = /^-?(([1-9]\d*)|0)(\.\d+)?((e|E)[+-]?\d+)?/
-  const output = input.match(regex)
-  if (output === null) {
+  const numRegex = /^-?(([1-9]\d*)|0)(\.\d+)?((e|E)[+-]?\d+)?/
+  const numOutput = input.match(numRegex)
+  if (numOutput === null) {
     return null
   }
-  return [Number(output[0]), input.slice(output[0].length)]
+  return [Number(numOutput[0]), input.slice(numOutput[0].length)]
 }
 
-console.log(numberParser('123abc'))
+console.log('numberParser', numberParser('0x123'))
 
-//
+// string parser
+function strParser (input) {
+  let i = 0
+  let str = ''
+  const escapeCharacters = {
+    '"': '\"',
+    '\\': '\\',
+    '/': '\/',
+    'b': '/b',
+    'f': '/f',
+    'n': '\n',
+    'r': '\r',
+    't': '\t',
+    'u':''
+  }
+  let char = input[i]
+  if (char !== '"') {
+    return null
+  }
+  while (input[i + 1] !== undefined) {
+    i += 1
+    char = input[i]
+    if (char === '"') { // check for end of string?
+      return [str, input.slice(i + 1)]
+    }
+    if (char === '\\') { // if back slash is encountered
+      i += 1
+      char = input[i]
+      if (!escapeCharacters.hasOwnProperty(char)) {
+        return null
+      } else if (char !== 'u') {
+        str += escapeCharacters[char]
+      } else if (char === 'u') {
+        const temp = input[i + 1] + input[i + 2] + input[i + 3] + input[i + 4]
+        str += String.fromCharCode(parseInt(temp, 16))
+        i += 4
+      }
+    } else {
+      str += char // if not a \ or "
+    }
+  }
+  return null
+}
+
+console.log('String parser', strParser('"..."'))
